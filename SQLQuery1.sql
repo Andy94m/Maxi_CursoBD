300fefdf75ba36f4eb046d88003cf577338d0468
Andy94m <m.andresmontes@gmail.com>
2025-06-18 17:49:45 -0300

    ajustamos sentencia primary

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,7 +1,18 @@
 Use master
 GO
 
-create database MundoPokemon_DB ON (
-	NAME='MundoPokemonDB',
-	FILENAME=''
+CREATE DATABASE MundoPokemon_DB
+ON PRIMARY (
+    NAME = 'MundoPokemonDB',
+    FILENAME = 'C:\Users\Andy\Desktop\MaxiPrograma\Curso Base de Datos\DB\MundoPokemonDB.mdf',
+    SIZE = 10MB,
+    MAXSIZE = 500MB,
+    FILEGROWTH = 100MB
+)
+LOG ON (
+    NAME = 'MundoPokemonDB_log',
+    FILENAME = 'C:\Users\Andy\Desktop\MaxiPrograma\Curso Base de Datos\DB\MundoPokemonDB_log.ldf',
+    SIZE = 5MB,
+    MAXSIZE = 100MB,
+    FILEGROWTH = 10MB
 );
